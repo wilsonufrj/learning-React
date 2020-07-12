2e90492c53c4bc87669cf6682664360e0830259d
Wilson <waramos97@gmail.com>
2020-07-12 11:26:40 -0300

    Estrutura do card completa e animação completa

--- src/pages/PokemonPage/index.js
+++ src/pages/PokemonPage/index.js
@@ -15,6 +15,24 @@ import {Card,CardBody,CardText,CardTitle,CardSubtitle,Button,CardImg,Badge} from
 const PokeURL = 'https://pokeapi.co/api/v2/pokemon?limit=1'
 
 
+const RenderCard = (pokemon,index)=>{
+    return (
+        <div className='col-3 mt-2 mb-2' key={index} >
+            <Card className='' >
+                <CardImg top className='' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} />
+                <CardBody className='text-center'>
+                    <CardTitle className='font-weight-bold'>#{index + 1} {pokemon.data.name}</CardTitle>
+                    <div className='row'>
+                        <Badge className='col-5 offset-1' color="primary ">Grass</Badge>
+                        <Badge className='ml-1 col-5' color="success"></Badge>
+                    </div>
+                    <Button>Button</Button>
+                </CardBody>
+            </Card>
+        </div>
+    )
+}
+
 
 const PokemonPage = () => {
 
@@ -69,20 +87,29 @@ const PokemonPage = () => {
                         data.loading ?
                             <p>Loading ...</p> :
                             pokemon.map((pokemon, index) => {
-                                return (
-                                    <div className='col-3 mt-2 mb-2' key={index} >
-                                        <Card className='' >
-                                            <CardImg top className='' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} />
-                                            <CardBody className='text-center'>
-                                                <CardTitle className='font-weight-bold'>#{index + 1} {pokemon.data.name}</CardTitle>
-                                                <div className='row'>
-                                                    <Badge className='col-5 offset-1' color="primary ">Grass</Badge>
-                                                    <Badge className='ml-1 col-5' color="success"></Badge>
-                                                </div>
-                                                <Button>Button</Button>
-                                            </CardBody>
-                                        </Card>
-                                    </div>
+                                return(
+                                    <div className='row m-3'>
+                                        <div className='card  text-center'  id='card'>
+                                            <img className='card-img-top' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png'/>
+                                            <div className='card-body'>
+                                                <h4 className='card-title'>#3 Venossaur</h4>
+                                                <h6 className='badge badge-success'>Grass</h6>
+                                                <h6 className='badge badge-info ml-1'>Poison</h6>
+                                                <Button className='btn btn-danger btn-block '>More</Button>
+                                            </div> 
+                                        </div>
+
+                                        <div className='card ml-2 text-center' id='card'>
+                                            <img className='card-img-top' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png'/>
+                                            <div className='card-body'>
+                                                <h4 className='card-title'>#6 Charizard</h4>
+                                                <h6 className='badge badge-danger'>Fire</h6>
+                                                <Button className='btn btn-danger btn-block'>More</Button>
+                                            </div>
+                                        </div>
+
+                                </div>
+                                
                                 )
                             })
                     }
